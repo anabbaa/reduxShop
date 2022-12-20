c9ee8b08779334a86df5a43129ef2ad050bc2eab
anabbaa <ahmed.nabaa.an@gmail.com>
2022-12-20 17:40:58 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/CartActions';
+
+
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  // useEffect(() => {
+  //   const sendCartData = async () => {
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'pending',
+  //         title: 'Sending...',
+  //         message: 'Sending cart data!',
+  //       })
+  //     );
+  //     const response = await fetch(
+  //       'https://react-http-6b4a6.firebaseio.com/cart.json',
+  //       {
+  //         method: 'PUT',
+  //         body: JSON.stringify(cart),
+  //       }
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error('Sending cart data failed.');
+  //     }
+
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'success',
+  //         title: 'Success!',
+  //         message: 'Sent cart data successfully!',
+  //       })
+  //     );
+  //   };
+    /* this only to be sure not sending when initalizoing appjs and define the variable before
+    app function true then return if it is true before sending data and change its value
+    to false so it will happen once */
+
+  //   if (isInitial) {
+  //     isInitial = false;
+  //     return;
+  //   }
+
+  //   sendCartData().catch((error) => {
+  //     dispatch(
+  //       uiActions.showNotification({
+  //         status: 'error',
+  //         title: 'Error!',
+  //         message: 'Sending cart data failed!',
+  //       })
+  //     );
+  //   });
+  // }, [cart, dispatch]);
+
+  // action creator solution
+
+  /*with this code we have another issue is that when we fetch data we canged and when cart
+  changed the useEfeect of sending data trigger again 
+  - solution add variaböe false to initial value change them to true so replacecart
+  method will still false then in app if only varaible is true we will senddata
+  the we will face a small issue we will send this variable with false to data base 
+  the solution is the function of sending data i will send object with items and total quantity
+  - another assue is that when i fetch data and database is empty it will crash my app because
+  it will be define the solution either some data or empty array  in replacement method
+  */
+
+  useEffect(()=>{
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed){
+      dispatch(sendCartData(cart));
+    }}, [cart, dispatch]);
+
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
---
+++ src/store/CartActions.js
@@ -0,0 +1,89 @@
+import { uiActions } from './UISlice';
+import { cartActions } from './CartSlice';
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://meals-3c964-default-rtdb.firebaseio.com//cart1.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Could not fetch cart data!');
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          //the second stiill part of database so it does not need or an empty array
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Fetching cart data failed!',
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://meals-3c964-default-rtdb.firebaseio.com//cart1.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify({
+            // this only not to send changed variable
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    }
+  };
+};
